b5eff63750a808881cdf5c1d12309f3dd39a6615
leedaohuan <379198689@qq.com>
2023-08-18 00:25:22 +0800

    feat: 处理HTML属性相关设置

--- renderer.js
+++ renderer.js
@@ -5,9 +5,9 @@ const { effect, ref } = VueReactivity;
  */
 function createRenderer(options) {
   // 通过 options 得到操作 DOM 的 API
-  const { createElement, insert, setElementText } = options;
+  const { createElement, insert, setElementText, patchProps } = options;
   /**
-   * patch函数
+   * patch函数，挂载或者更新节点
    * @param {} n1 旧node
    * @param {} n2 新node
    * @param {} container 容器
@@ -24,10 +24,23 @@ function createRenderer(options) {
   const mountElement = (vnode, container) => {
     // 创建 DOM 元素
     const el = createElement(vnode.type);
+
     // 处理子节点，如果子节点是字符串，代表元素具有文本节点
     if (typeof vnode.children === "string") {
       // 因此只需要设置元素的 textContent 属性即可
       setElementText(el, vnode.children);
+    } else if (Array.isArray(vnode.children)) {
+      // 如果children 是数组， 则遍厉每一个子节点，并调用 patch 函数挂载它们
+      vnode.children.forEach((child) => {
+        patch(null, child, el);
+      });
+    }
+    // 如果 vnode.props 存在才处理它
+    if (vnode.props) {
+      // 遍历 vnode.props
+      for (const key in vnode.props) {
+        patchProps(el, key, null, vnode.props[key]);
+      }
     }
     // 将元素添加到容器中
     insert(el, container);
@@ -49,6 +62,18 @@ function createRenderer(options) {
   return { render };
 }
 
+/**
+ * prop是否应该被设置（有些html标签的属性是只读的）
+ * @param {*} el
+ * @param {*} key
+ * @param {*} value
+ */
+const shouldSetAsProps = (el, key, value) => {
+  // 特殊处理
+  if (key === "form" && el.tagName === "INPUT") return false;
+  return key in el;
+};
+
 const renderer = createRenderer({
   createElement(tag) {
     return document.createElement(tag);
@@ -61,12 +86,36 @@ const renderer = createRenderer({
   insert(el, parent, anchor = null) {
     parent.insertBefore(el, anchor);
   },
+  patchProps(el, key, preValue, value) {
+    // 使用 in 操作符判断 key 是否存在对应的 DOM Properties
+    if (shouldSetAsProps(el, key, value)) {
+      // 使用 shouldSetAsProps 函数判断是否应该作为 DOM Properties 设置
+      const type = typeof el[key];
+      // 如果是布尔类型，并且 value 是空字符串，则将值矫正为 true
+      if (type === "boolean" && value === "") {
+        el[key] = true;
+      } else {
+        el[key] = value;
+      }
+    } else {
+      // 如果要设置的值属性没有对应的 DOM properties， 则使用setAttribute 函数设置
+      el.setAttribute(key, value);
+    }
+  },
 });
 
 const vnode = {
-  type: "h1",
-  children: "hello world",
+  type: "div",
+  // 使用 prop 描述一个元素的属性
+  props: {
+    id: "foo1",
+  },
+  children: [
+    {
+      type: "p",
+      children: "hello world",
+    },
+  ],
 };
-
 
 renderer.render(vnode, document.querySelector("#app"));
