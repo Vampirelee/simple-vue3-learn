271f7c1a560d661bf107c5d1408d12bfe109bfef
leedaohuan <379198689@qq.com>
2023-08-23 21:24:29 +0800

    feat: [renderer]事件绑定

--- renderer.js
+++ renderer.js
@@ -108,24 +108,56 @@ const renderer = createRenderer({
   insert(el, parent, anchor = null) {
     parent.insertBefore(el, anchor);
   },
-  patchProps(el, key, preValue, value) {
+  patchProps(el, key, prevValue, nextValue) {
+    // 匹配以 on 开头的属性， 视为事件
+    if (/^on/.test(key)) {
+      // 获取为该元素伪造的事件处理函数 invoker （vei是 vue event invoker的首字母缩写）
+      const invokers = el._vei || (el._vei = {});
+      let invoker = invokers[key];
+      // 根据属性名称得到对应的事件名称，例如 onClick ---->  click
+      const name = key.slice(2).toLowerCase();
+      if (nextValue) {
+        if (!invoker) {
+          // 如果没有 invoker，则将一个伪造的 invoker 缓存到 el._vei中
+          invoker = el._vei[key] = (e) => {
+            // 如果 invoker.value是数组，则遍历它并逐个调用事件处理函数
+            if (Array.isArray(invoker.value)) {
+              invoker.value.forEach((fn) => fn(e));
+            } else {
+              // 否则直接调用
+              invoker.value(e);
+            }
+          };
+          // 将真正的事件处理函数赋值给 invoker.value
+          invoker.value = nextValue;
+          // 绑定 invoker 作为事件处理函数
+          el.addEventListener(name, invoker);
+        } else {
+          // 如果 invoker 存在，意味着更新，并且只需要更新 invoker.value 的值即可
+          invoker.value = nextValue;
+        }
+      } else if (invoker) {
+        // 新的事件绑定函数不存在，且之前绑定的 invoker 存在，则移除绑定
+        el.removeEventListener(name, invoker);
+      }
+    }
     // 对 class 类进行特殊处理（el.className、setAttribute 和 el.classList这三个方法都可以设置HTML的class属性，但经过测试 el.className性能最优）
-    if (key === "class") {
-      el.className = value || "";
+    else if (key === "class") {
+      el.className = nextValue || "";
     }
     // 使用 in 操作符判断 key 是否存在对应的 DOM Properties
-    if (shouldSetAsProps(el, key, value)) {
+    else if (shouldSetAsProps(el, key, nextValue)) {
       // 使用 shouldSetAsProps 函数判断是否应该作为 DOM Properties 设置
       const type = typeof el[key];
-      // 如果是布尔类型，并且 value 是空字符串，则将值矫正为 true
-      if (type === "boolean" && value === "") {
+      // 如果是布尔类型，并且 nextValue 是空字符串，则将值矫正为 true
+      if (type === "boolean" && nextValue === "") {
         el[key] = true;
       } else {
-        el[key] = value;
+        el[key] = nextValue;
       }
     } else {
       // 如果要设置的值属性没有对应的 DOM properties， 则使用setAttribute 函数设置
-      el.setAttribute(key, value);
+      el.setAttribute(key, nextValue);
     }
   },
 });
