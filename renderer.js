2a487caad11d5d2938e8e7a2556b481f49fdf01e
leedaohuan <379198689@qq.com>
2023-12-13 21:07:36 +0800

    feat: 组件的渲染上下文

--- renderer.js
+++ renderer.js
@@ -189,33 +189,60 @@ function createRenderer(options) {
     // 将组件实例设置到 vnode 上，用于后续更新
     vnode.component = instance;
 
+    // 创建渲染上下文对象，本质上是组件实例的代理
+    const renderContext = new Proxy(instance, {
+      get(t, k, r) {
+        // 取得组件自身状态与 props 数据
+        const { state, props } = t;
+        // 先尝试读取自身状态数据
+        if (state && k in state) {
+          return state[k];
+        } else if (k in props) {
+          // 如果组件自身没有该数据，则尝试从 props 中读取
+          return props[k];
+        } else {
+          console.error("不存在");
+        }
+      },
+      set(t, k, v, r) {
+        const { state, props } = t;
+        if (state && k in state) {
+          state[k] = v;
+        } else if (k in props) {
+          console.warn(`Attempting to mutate prop "${k}". Props are readonly.`);
+        } else {
+          console.error("不存在");
+        }
+      },
+    });
+
     // 在这里调用 created 钩子
-    created && created();
+    created && created.call(renderContext);
 
     // 将组件的 render 函数调用包装到 effect 内
     effect(
       () => {
         // 调用组件的渲染函数，获得子树
-        const subTree = render.call(state, state);
+        const subTree = render.call(renderContext, state);
         // 检查组件是否已经被挂载
         if (!instance.isMounted) {
           // 这里调用 beforeMount 钩子
-          beforeMount && beforeMount.call(state);
+          beforeMount && beforeMount.call(renderContext);
 
           // 初次挂载，调用 patch 函数第一个参数传递 null
           patch(null, subTree, container, anchor);
           // 重点： 将组件实例的 isMounted 设置为true，这样当更新发生时就不会再次进行挂载操作，而是会执行更新
           instance.isMounted = true;
           // 在这里调用 mounted 钩子
-          mounted && mounted.call(state);
+          mounted && mounted.call(renderContext);
         } else {
           // 在这里调用 beforeUpdate 钩子
-          beforeUpdate && beforeUpdate.call(state);
+          beforeUpdate && beforeUpdate.call(renderContext);
           // 当 isMountd 为true时，说明组件已经被挂载，只需要完成自更新即可，
           // 所以在调用 patch 函数时，第一个参数为组件上一次渲染的子树，意思是使用新的子树与上一次渲染的子树进行补丁操作
           patch(instance.subTree, subTree, container, anchor);
           // 在这里调用 updated 钩子
-          updated && updated.call(state);
+          updated && updated.call(renderContext);
         }
         // 更新组件实例的子树
         instance.subTree = subTree;
