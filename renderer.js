3c05598b6de2d980d53bae86cc0cfa10f9a1125c
leedaohuan <379198689@qq.com>
2023-12-18 23:24:47 +0800

    feat: KeepAlive组件原理

--- renderer.js
+++ renderer.js
@@ -59,6 +59,58 @@ function onMounted(fn) {
   }
 }
 
+// KeepAlive 组件
+const KeepAlive = {
+  // KeepAlive 组件独有的属性，用作标识
+  __isKeepAlive: true,
+  setup(props, { slots }) {
+    // 创建一个缓存对象
+    // key: vnode.type
+    // value: vnode
+    const cache = new Map();
+    // 当前KeepAlive组件的实例
+    const instance = currentInstance;
+    // 对于KeepAlive组件来说，它的实例上存在特殊的 keepAliveCtx 对象，该对象由渲染器注入，该对象会暴露渲染器
+    // 的一些内部方法，其中 move 函数用来将一段 DOM 移动到另一个容器中
+    const { move, createElement } = instance.keepAliveCtx;
+
+    // 创建隐藏容器
+    const storageContainer = createElement("div");
+    // KeepAlive 组件的实例上会被添加两个内部函数，分别是 _deActivate 和 _activate
+    // 这两个函数会在渲染器中被调用
+    instance._deActivate = (vnode) => {
+      move(vnode, storageContainer);
+    };
+    instance._activate = (vnode, container, anchor) => {
+      move(vnode, container, anchor);
+    };
+    return () => {
+      // KeepAlive 的默认插槽就是要被 KeepAlive 的组件
+      let rawVNode = slots.default();
+      // 如果不是组件，直接渲染即可，因为非组件的虚拟节点无法被 KeepAlive
+      if (typeof rawVNode.type !== "object") {
+        return rawVNode;
+      }
+      // 在挂载时先获取缓存的组件 vnode
+      const cachedVNode = cache.get(rawVNode.type);
+      if (cachedVNode) {
+        // 如果有缓存的内容，则说明不应该执行挂载，而应该执行激活，继承组件实例
+        rawVNode.component = cachedVNode.component;
+        rawVNode.keptAlive = true;
+      } else {
+        // 如果没有缓存，则将其添加到缓存中，这样下次激活组件时就不会执行新的挂载动作了
+        cache.set(rawVNode.type, rawVNode);
+      }
+      // 在组件 vnode 上添加 shouldKeepAlive 属性，并标记为true，避免渲染器真的将组件卸载
+      rawVNode.shouldKeepAlive = true;
+      // 将 KeepAlive 组件的实例也添加到 vnode 上，以便在渲染器中访问
+      rawVNode.keepAliveInstance = instance;
+      // 渲染组件 vnode
+      return rawVNode;
+    };
+  },
+};
+
 // defineAsyncComponent 函数用于定义一个异步组件，接收一个异步组件加载器作为参数
 function defineAsyncComponent(options) {
   // options 可以是配置项，也可以是加载器, 这里的技巧，参数归一
@@ -223,8 +275,13 @@ function createRenderer(options) {
     else if (typeof type === "object" || typeof type === "function") {
       // vnode.type 的值是选项对象，作为组件来处理
       if (!n1) {
-        // 挂载组件
-        mountComponent(n2, container, anchor);
+        // 如果该组件已经被 KeepAlive， 则不会重新挂载它， 而是会调用 _activate 来激活它
+        if (n2.keptAlive) {
+          n2.keepAliveInstance._activate(n2, container, anchor);
+        } else {
+          // 挂载组件
+          mountComponent(n2, container, anchor);
+        }
       } else {
         // 更新组件
         patchComponent(n1, n2, container);
@@ -338,8 +395,23 @@ function createRenderer(options) {
       slots,
       // 在组件实例中添加 mounted 数据，用来存储通过 onMounted 函数注册的生命周期钩子函数
       mounted: [],
+      // 只有 KeepAlive 组件的实例下会有 keepAliveCtx 属性
+      keepAliveCtx: null,
     };
 
+    // 检查当前要挂载的组件是否是 KeepAlive 组件
+    const isKeepAlive = vnode.type.__isKeepAlive;
+    if (isKeepAlive) {
+      // 在 KeepAlive 组件实例上添加 keepAliveCtx 对象
+      instance.keepAliveCtx = {
+        // move 函数用来移动一段 vnode
+        move(vnode, container, anchor) {
+          // 本质上是将组件渲染的内容移动到指定容器中，即隐藏容器中
+          insert(vnode.component.subTree.el, container, anchor);
+        },
+        createElement,
+      };
+    }
     // 将 attrs、emit、slots 对象添加到setupContext中
     const setupContext = { attrs, emit, slots };
     // 在调用 setup 函数之前，设置当前组件实例
@@ -864,8 +936,14 @@ function createRenderer(options) {
     if (vnode.type === Fragment) {
       vnode.children.forEach((c) => unmount(c));
     } else if (typeof vnode.type === "object") {
-      // 对于组件的卸载，本质上是要卸载所渲染的内容， 即 subTree
-      unmount(vnode.component.subTree);
+      // vnode.shouldKeepAlive 是一个布尔值，用来标识该组件是否应该被 KeepAlive
+      if (vnode.shouldKeepAlive) {
+        // 对于需要被 KeepAlive 的组件，我们不应该真的卸载它，而应调用该组件的父组件，即 KeepAlive 组件的 _deActivate 函数使其失活
+        vnode.keepAliveInstance._deActivate(vnode);
+      } else {
+        // 对于组件的卸载，本质上是要卸载所渲染的内容， 即 subTree
+        unmount(vnode.component.subTree);
+      }
       return;
     }
     const parent = vnode.el.parentNode;
