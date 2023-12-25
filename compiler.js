b9d5bfb830dbc70da4da763af49d64e5bee99079
leedaohuan <379198689@qq.com>
2023-12-25 22:35:29 +0800

    feat: [compiler]节点上下文

--- compiler.js
+++ compiler.js
@@ -192,23 +192,74 @@ function parse(str) {
   return root;
 }
 
+function transformElement(node, context) {
+  if (node.type === "Element" && node.tag === "p") {
+    node.tag = "h1";
+  }
+}
+function transformText(node, context) {
+  if (node.type === "Text") {
+    node.content = node.content.repeat(2);
+    context.removeNode();
+  }
+}
+
+// transform 函数
+function transform(ast) {
+  // 在 transform 函数内创建 context 对象
+  const context = {
+    // 增加 currentNode， 用来存储当前正在转换的节点
+    currentNode: null,
+    // 增加 childIndex 用来存储当前节点在父节点的 children 中的位置索引
+    childIndex: 0,
+    // 增加 parent，用来存储当前转换节点的父节点
+    parent: null,
+    // 用于替换节点的函数，接收新节点作为参数
+    replaceNode(node) {
+      context.parent.children[context.childIndex] = node;
+      context.currentNode = node;
+    },
+    // 用于删除当前节点
+    removeNode() {
+      if (context.parent) {
+        // 调用数组的 splice 方法，根据当前节点的索引删除当前节点
+        context.parent.children.splice(context.childIndex, 1);
+        // 将 context.currentNode 置空
+        context.currentNode = null;
+      }
+    },
+    // 注册 nodeTransform 数组
+    nodeTransforms: [transformElement, transformText],
+  };
+  // 调用 traverseNode 完成转换
+  traverseNode(ast, context);
+  // 打印 AST 信息
+  dump(ast);
+}
 // traverseNode 函数
 function traverseNode(ast, context) {
   // 当前节点，ast本身就是 Root 节点
-  const currentNode = ast;
+  // 设置当前转换的节点信息 context.currentNode
+  context.currentNode = ast;
   // context.nodeTransforms 是一个数组，其中每一个元素都是一个函数
   const transforms = context.nodeTransforms;
   for (let i = 0; i < transforms.length; i++) {
     // 将当前节点 currentNode 和 context 都传递给 nodeTransforms 中注册的回调函数
     const t = transforms[i];
-    t(currentNode, context);
+    t(context.currentNode, context);
+    // 由于任何转换函数都可能移除当前节点，因此每个转换函数执行完毕后，都应该检查当前节点是否已经被移除，如果被移除了，直接返回即可
+    if (!context.currentNode) return;
   }
   // 如果有子节点，则递归地调用 traverseNode 函数进行遍历
-  const children = currentNode.children;
+  const children = context.currentNode.children;
   if (children) {
     for (let i = 0; i < children.length; i++) {
+      // 递归地调用 traverseNode 转换子节点之前，将当前节点设置为父节点
+      context.parent = context.currentNode;
+      // 设置位置索引
+      context.childIndex = i;
       const node = children[i];
-      traverseNode(node);
+      traverseNode(node, context);
     }
   }
 }
@@ -235,5 +286,5 @@ function dump(node, indent = 0) {
 
 const vueTemplate =
   "<div><p><span>Vue</span><span>React</span></p><p>Template</p></div>";
-console.log(JSON.stringify(parse(vueTemplate), null, 2));
-dump(parse(vueTemplate));
+
+transform(parse(vueTemplate));
