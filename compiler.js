d20fc0f4a6e7ee22f26015fffdcfda00184759cf
leedaohuan <379198689@qq.com>
2024-01-04 23:26:52 +0800

    feat: [compiler]解析文本注释

--- compiler.js
+++ compiler.js
@@ -324,6 +324,26 @@ function parseInterpolation(context) {
     },
   };
 }
+// 解析文本注释
+function parseComment(context) {
+  // 消费注释开始部分
+  context.advanceBy("<!--".length);
+  // 找到注释结束部分的位置索引
+  closeIndex = context.source.indexOf("-->");
+  // 截取注释节点的内容
+  const content = context.source.slice(0, closeIndex);
+
+  // 消费内容
+  context.advanceBy(content.length);
+
+  // 消费注释的结束部分
+  context.advanceBy("-->".length);
+  // 返回类型为 Comment 的节点
+  return {
+    type: "Comment",
+    content,
+  };
+}
 
 // 解析文本节点
 function parseText(context) {
