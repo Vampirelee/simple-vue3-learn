3ca16b0189f91defd713b652bf45c2d66c7aa427
leedaohuan <379198689@qq.com>
2024-01-04 23:03:29 +0800

    feat: [compiler] 解析html实体

--- compiler.js
+++ compiler.js
@@ -5,6 +5,16 @@ const TextModes = {
   RAWTEXT: "RAWTEXT",
   CDATA: "CDATA",
 };
+
+// 实体引用表
+const namedCharacterReferences = {
+  gt: ">",
+  "gt;": ">",
+  lt: "<",
+  "lt;": "<",
+  "ltcc;": "⪦",
+  // ... 还有其他实体
+};
 // 定义状态机状态
 const State = {
   initial: 1, // 初始状态
@@ -290,7 +300,119 @@ function parseElement(context, ancestors) {
 function parseInterpolation(context) {}
 
 // 解析文本节点
-function parseText(context) {}
+function parseText(context) {
+  // endIndex 为文本内容的结尾索引，默认将整个模板剩余内容都作为文本内容
+  let endIndex = context.source.length;
+
+  // 寻找字符 < 的位置索引
+  const ltIndex = context.source.indexOf("<");
+  // 寻找定界符 {{ 的位置索引
+  const delimiterIndex = context.source.indexOf("{{");
+
+  if (ltIndex > -1 && ltIndex < endIndex) endIndex = ltIndex;
+
+  if (delimiterIndex > -1 && delimiterIndex < endIndex)
+    endIndex = delimiterIndex;
+
+  // 此时 endIndex 是最终的文本内容的结尾索引，调用 slice 函数截取文本内容
+  const content = context.source.slice(0, endIndex);
+  context.advanceBy(content.length);
+
+  return {
+    // 节点类型
+    type: "Text",
+    content,
+  };
+}
+
+// 解析实体函数
+function decodeHtml(rawText, asAttr = false) {
+  let offset = 0;
+  const end = rawText.length;
+  // 经过解码后的文本将作为返回值被返回
+  let decodeText = "";
+  // 引用表中实体名称的最大长度
+  let maxCRNameLength = 0;
+
+  // advance 函数用于消费指定长度的文本
+  function advance(length) {
+    offset += length;
+    rawText = rawText.slice(length);
+  }
+
+  // 消费字符串，直到处理完毕
+  while (offset < end) {
+    // 用于匹配字符引用的开始部分，如果匹配成功，那么 head[0] 的值将有三种可能
+    // 1. head[0] === '&' 该字符引用是命名字符引用
+    // 2. head[0] === '&#' 该字符引用是用十进制表示的数字字符引用
+    // 2. head[0] === '&#x' 该字符引用是用十六进制表示的数字字符引用
+    const head = /&(?:#x?)?/i.exec(rawText);
+    if (!head) {
+      // 计算剩余内容的长度
+      const remaining = end - offset;
+      // 将剩余内容加到 decodedText 上
+      decodedText += rawText.slice(0, remaining);
+      // 消费剩余内容
+      advance(remaining);
+      break;
+    }
+    // head.index 为匹配的字符 & 在 rawText 中的位置索引
+    // 截取字符 & 之前的内容加到 decodedText 上
+    decodeText += rawText.slice(0, head.index);
+
+    // 消费字符 & 之前的内容
+    advance(head.index);
+
+    // 如果满足条件，则说明是命名字符引用，否则为数字字符引用
+    if (head[0] === "&") {
+      let name = "";
+      let value;
+      // 字符 & 的下一个字符必须是ASCII 字母或数字，这样才是合法的命名字符引用
+      if (/[0-9a-z]/i.test(rawText[1])) {
+        // 根据引用表计算实体名称的最大长度，
+        if (!maxCRNameLength) {
+          maxCRNameLength = Object.keys(namedCharacterReferences).reduce(
+            (max, name) => Math.max(max, name.length),
+            0
+          );
+        }
+
+        // 从最大长度开始对文本进行截取，并试图去引用表中找到对应的项
+        for (let length = maxCRNameLength; !value && length > 0; length--) {
+          // 截取字符 & 到最大长度之间的字符作为实体名称
+          name = rawText.substr(1, length);
+          // 使用实体名称去索引表中查找对应项的值
+          value = namedCharacterReferences[name];
+        }
+        // 如果找到了对应项的值，说明解码成功
+        if (value) {
+          // 检查实体名称的最后一个匹配字符是否是分号
+          const semi = name.endsWith(";");
+          // 如果解码的文本作为属性值，最后一个匹配的字符不是分号，并且最后一个匹配字符的下一个字符是等于号、ASCII字母或数字
+          // 由于历史原因，将字符 & 和实体名称 name 作为普通文本
+          if (
+            asAttr &&
+            !semi &&
+            /[=a-z0-9]/i.test(rawText[name.length + 1] || "")
+          ) {
+            decodeText += "&" + name;
+            advance(1 + name.length);
+          } else {
+            // 其他情况下，正常使用解码后的内容拼接到 decodedText 上
+            decodeText += value;
+            advance(1 + name.length);
+          }
+        } else {
+          // 如果字符 & 的下一个字符不是ASCII 字母或数字，则将字符 & 作为普通文本
+          decodeText += "&";
+          advance(1);
+        }
+      }
+    }
+  }
+
+  return decodeText;
+}
 
 function parseChildren(context, ancestors) {
   // 定义 nodes 数组存储子节点，它将作为最终的返回值
@@ -337,8 +459,9 @@ function isEnd(context, ancestors) {
     // 获取祖先标签节点（为了更好的提示，这里不找栈顶父级节点做比较）
     const ancestor = ancestors[i];
     // 如果遇到结束标签，并且该标签与父级标签节点同名，则停止
-    if (context.source.startsWith(`</${ancestor.tag}>`)) return true;
+    if (context.source.startsWith(`</${ancestor.tag}`)) return true;
   }
+  return false;
 }
 
 function parse(str) {
